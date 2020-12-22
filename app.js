abdd74c4d97dd41bd07c264443cc8b23aa2747eb
Tyler Thompson <studiesweekly@Tylers-MacBook-Pro.local>
2020-12-22 15:01:39 -0700

    add favicon middleware

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+const favicon = require('serve-favicon')
+const path = require('path')
 require('dotenv/config');
 
 const PORT = 3021;
 
 const app = express();
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
+
 const {SERVER_PORT, GMAIL_USER, GMAIL_PASS} = process.env
 
 app.use(express.static("public"))
